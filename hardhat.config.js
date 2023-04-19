8c87a757554ea87a5f69833029f5e798ae504c39
yuk6ra <yuki.imamura.all@gmail.com>
2023-04-19 18:46:34 +0900

    Update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+    networks: {
+        mumbai: {
+            url: process.env.ALCHEMY_MUMBAI_KEY,
+            accounts: [process.env.PRIVATE_KEY]
+        },
+    },
+    etherscan: {        
+        apiKey: process.env.POLYGONSCAN_API_KEY
+    }
 };
